d869fddf78fafb99dbd66a9fea2795e10057cc7d
kumar harsh <harsh@kumars-MacBook-Air.local>
2024-11-03 18:45:18 +0530

    hindi story

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,7 +1,8 @@
 import edge_tts
 
-async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+async def generate_audio(text, outputFilename):
+    # Use a Hindi voice
+    communicate = edge_tts.Communicate(text, "hi-IN-SwaraNeural")
     await communicate.save(outputFilename)
 
 
